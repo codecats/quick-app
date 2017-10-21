5b68ec6920753b37bb576244b27f6182c2e14bd4
t <t@localhost.localdomain>
2017-10-21 12:42:11 +0200

    readme fix

--- resources/assets/src/Hello.vue
+++ resources/assets/src/Hello.vue
@@ -1,15 +1,14 @@
 <template>
   <div class="container">
     <div class="notification">
-      This container is <strong>centered</strong> on desktop. {{ msg }}
-
-      
-  Danger
-<a class="button is-primary">Primary</a>
-<a class="button is-info">Info</a>
-<a class="button is-success">Success</a>
-<a class="button is-warning">Warning</a>
-<a class="button is-danger">Danger</a>
+      Feel the power of the future <strong>{{ counter }}</strong>
+      <br>
+      Some tools:
+<a class="button is-primary" @click="counter++">👍</a>
+<a class="button is-info" @click="counter*=2"><i class="fa fa-handshake-o" aria-hidden="true"></i></a>
+<a class="button is-success" @click="counter += 100"><i class="fa fa-fighter-jet" aria-hidden="true"></i></a>
+<a class="button is-warning" style="color: white" @click="counter *= counter"><i class="fa fa-bolt" aria-hidden="true"></i></a>
+<a class="button is-danger" @click="alarm()">Cool!</a>
     </div>
   </div>
 
@@ -20,8 +19,12 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      counter: 0
     }
+  },
+  methods: {
+    alarm () {alert(this.counter);}
   }
 }
 </script>
